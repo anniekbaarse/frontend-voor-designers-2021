c77c1f52b54a6ef7727dc9c09ac3336e80264657
anniekbaarse <74059379+anniekbaarse@users.noreply.github.com>
2021-02-16 23:06:29 +0100

    eindresultaat

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -5,40 +5,69 @@
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
 
+/* alle button met class 'wish' in de HTML opzoeken */
+/* omdat er meer buttons geselecteerd moeten worden, wordt querySelectorAll gebruikt (en niet querySelector) */
+/* het gevonden lijstje met buttons (een array) in de variabele 'hearts' opslaan */
+var hearts = document.querySelectorAll("button.wish");
 
-/* favorieten hartje */
-var favorietenLeeg = document.getElementById('favorietenLeeg');
-var favorietenVol = document.getElementById('favorietenVol');
+/* elke button laten luisteren naar een klik */
+/* na een klik de functie 'toggleLove' uitvoeren */
+hearts[0].addEventListener("click", addToFavorites);
+hearts[1].addEventListener("click", addToFavorites);
+hearts[2].addEventListener("click", addToFavorites);
+hearts[3].addEventListener("click", addToFavorites);
 
-function hartjeGevuld() {
-    favorietenLeeg.classList.toggle('geklikt');
-    // zodat hartje weer leeg wordt //
-    favorietenVol.classList.remove('geklikt');
-    // kijken of het hartje gevuld is of niet //
+/* liefde declareren of liefde wegnemen */
+function addToFavorites(event) {
+  /* het hartje waarop geklikt is in de variabele 'clickedHeart' opslaan */
+  var clickedHeart = event.target;
+
+  /* als het hartje gebroken is */
+  if (clickedHeart.innerHTML == "💔") {
+    /* het gebroken hartje vervangen door het gele hartje */
+    clickedHeart.innerHTML = "💛";
+    /* en dan de functie aanroepen om de wishlist aan te passen */
+    /* in dit geval moet er liefde bij, vandaar 'plus' */
+    updateWishlist("plus");
+  }
+  /* als het hartje geel is */
+  else {
+    /* het gele hartje vervangen door het gebroken hartje */
+    clickedHeart.innerHTML = "💔";
+    /* en dan de functie aanroepen om de love-list aan te passen */
+    /* in dit geval moet er liefde af, vandaar 'min' */
+    updateWishlist("min");
+  }
 }
 
-function hartjeGeleegd() {
-    favorietenVol.classList.toggle('geklikt');
-    // zodat hartje weer leeg wordt //
-    favorietenLeeg.classList.remove('geklikt');
-    // kijken of het hartje gevuld is of niet //
+/* de Wishlist in de nav in de header updaten */
+/* de action die meegegeven wordt, is "plus" of "min" */
+function updateWishlist(action) {
+  /* het getal (de span) in de Wishlist-link in de nav opzoeken */
+  /* die span in de variabele 'loveListAmount' opslaan */
+  let wishlistAmount = document.querySelector(".wishlist span");
+
+  /* het huidige aantal gele hartjes bepalen */
+  let currentAmount = wishlistAmount.innerHTML;
+  /* dat is een string - dus even omzetten naar een getal */
+  currentAmount = parseInt(currentAmount);
+
+  /* het nieuwe aantal gele hartjes bepalen */
+  /* te beginnen met een variabele om het nieuwe aantal in op te slaan */
+  let newAmount;
+
+  /* als er liefde bij moet */
+  if (action == "plus") {
+    /* het nieuwe aantal gele hartjes berekenen */
+    newAmount = currentAmount + 1;
+  }
+
+  /* als er liefde af moet */
+  else {
+    /* het nieuwe aantal gele hartjes berekenen */
+    newAmount = currentAmount - 1;
+  }
+
+  /* tenslotte het nieuwe aantal in de HTML zetten */
+  wishlistAmount.innerHTML = newAmount;
 }
-
-//function duimOmlaagGeklikt() {
-//    duimOmlaag.classList.toggle('geklikt');
-//    // zodat duimpje weer leeg wordt //
-//    duimOmhoog.classList.remove('geklikt');
-//    // kijken of het duimpje gevuld is of niet //
-//
-//    if (duimOmlaag.classList.contains('geklikt')) {
-//        teller.innerHTML = '+8 likes'
-//    } else {
-//        teller.innerHTML = '+9 likes'
-//    }
-//}
-
-favorietenLeeg.addEventListener('click', hartjeGevuld);
-favorietenVol.addEventListener('click', hartjeGeleegd);
-
-
-// https://stackoverflow.com/questions/48900061/toggle-heart-shaped-html-code-in-javascript
