22705ae8273aed5b77fb357ac9b10afe82d222a1
anniekbaarse <74059379+anniekbaarse@users.noreply.github.com>
2021-03-17 14:12:00 +0100

    opdracht 2 - almost done

---
+++ opdracht2/demo/js/script.js
@@ -0,0 +1,26 @@
+function changeColour(value) {
+    var color = document.body.style.backgroundColor;
+    switch (value) {
+        case 'light':
+            color = "#FFF";
+            break;
+        case 'dark':
+            color = "#727272";
+            break;
+    }
+    document.body.style.backgroundColor = color;
+}
+
+// Bron: https://stackoverflow.com/questions/20102750/change-background-color-using-radio-buttons
+
+var ranger = document.getElementById('ranger');
+var image = document.getElementByClassName('image');
+var width = image.width;
+var height = image.height;
+
+ranger.onchange = function () {
+    image.width = width * (ranger.value / 100);
+    image.height = height * (ranger.value / 100);
+}
+
+// Bron: https://stackoverflow.com/questions/15634036/using-input-type-range-to-scale-an-image en http://jsfiddle.net/DnE83/1/
