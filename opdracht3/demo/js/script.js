4ec57c6d857b9142782dcae3cf9a82124d66a167
anniekbaarse <74059379+anniekbaarse@users.noreply.github.com>
2021-04-04 21:18:23 +0200

    Update script.js

--- opdracht3/demo/js/script.js
+++ opdracht3/demo/js/script.js
@@ -1,26 +1,44 @@
+// elementen ophalen uit de html
+const dog_btn = document.getElementById('dog_btn');
+const dog_result = document.getElementById('dog_result');
+
+// eventlistener toevoegen aan de button
+dog_btn.addEventListener('click', getRandomDog);
+
+// radio buttons voor achtergrondkleur
 function changeColour(value) {
+    // maak een variabele voor de kleur aan
     var color = document.body.style.backgroundColor;
     switch (value) {
+        // als light is geselecteerd, is de achtergrondkleur lichtgrijs
         case 'light':
-            color = "#FFF";
+            color = "lightgrey";
             break;
+            // als dark is geselecteerd, is de achtergrondkleur donkergrijs
         case 'dark':
             color = "#727272";
             break;
     }
+    // achtergrondkleur aanpassen naar de gelecteerde kleur
     document.body.style.backgroundColor = color;
 }
 
-// Bron: https://stackoverflow.com/questions/20102750/change-background-color-using-radio-buttons
-
-var ranger = document.getElementById('ranger');
-var image = document.getElementById('image');
-var width = image.width;
-var height = image.height;
-
-ranger.onchange = function () {
-    image.width = width * (ranger.value / 100);
-    image.height = height * (ranger.value / 100);
+// random hondenafbeelding toevoegen
+function getRandomDog() {
+    // api ophalen
+    fetch('https://random.dog/woof.json')
+        .then(res => res.json())
+        .then(data => {
+            // als de data een .mp4 bestand is nog een keer de functie doorlopen
+            if (data.url.includes('.mp4')) {
+                getRandomDog();
+            }
+            // als de data een img is, de source van de afbeelding toevoegen aan de html om de afbeelding te weergeven
+            else {
+                dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+            }
+        });
 }
 
-// Bron: https://stackoverflow.com/questions/15634036/using-input-type-range-to-scale-an-image en http://jsfiddle.net/DnE83/1/
+
+// Bron: https://www.youtube.com/watch?v=7f2HNadULOs
